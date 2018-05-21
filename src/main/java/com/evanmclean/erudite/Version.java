3d0d535cfa9dab1ef8a56e94ea6a8dc421623419
Evan McLean <evan@evanmclean.com>
2018-05-21 19:11:01 +1000

    Updated version number from 2.3 to 2.4.

--- src/main/java/com/evanmclean/erudite/Version.java
+++ src/main/java/com/evanmclean/erudite/Version.java
@@ -3,10 +3,11 @@ package com.evanmclean.erudite;
 /**
  * Just contains the version number for the application.
  *
- * @author Evan M<sup>c</sup>Lean, <a href="http://evanmclean.com/"
- *         target="_blank">M<sup>c</sup>Lean Computer Services</a>
+ * @author Evan M<sup>c</sup>Lean,
+ *         <a href="http://evanmclean.com/" target="_blank">M<sup>c</sup>Lean
+ *         Computer Services</a>
  */
 public interface Version
 {
-  public static final String VERSION = "2.3";
+  public static final String VERSION = "2.4";
 }
